f1d800da0080923d3dd5beb8ef99e303bfd0851a
Brandon Levy <brandon.levy@mail.mcgill.ca>
2017-11-21 14:02:31 -0500

    made kitten picture size

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,4 +12,9 @@ class PagesController < ApplicationController
     @header = "Here is the contest"
   end
 
+  def kitten
+    requested_size = params[:size]
+    @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+  end
+
 end
