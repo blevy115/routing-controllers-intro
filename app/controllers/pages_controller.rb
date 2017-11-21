a0b5134d3757def9887d4c36bd595308bd94b673
Brandon Levy <brandon.levy@mail.mcgill.ca>
2017-11-21 13:50:49 -0500

    made changable instance variable header for methods

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,15 @@
 class PagesController < ApplicationController
 
   def welcome
-    # render :secrets
+    @header = "This is the Welcome page"
+  end
+
+  def about
+    @header = "This is the about page"
+  end
+
+  def contest
+    @header = "Here is the contest"
   end
 
 end
